360bfd541472e24d4cafcd01eb3081666876f987
Vlad Goenko <vlad.goenko@gmail.com>
2019-06-02 17:22:08 +0300

    added procfile, scheduler logic, flush for prints in console

--- scanner.py
+++ scanner.py
@@ -1,5 +1,20 @@
 from sources import get_response
 from parsers import get_flights_details
+import sys
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=10)
+def timed_job():
+    source = get_response(pattern_url)
+    flights = get_flights_details(source)
+    print(flights)
+    sys.stdout.flush()
+
+
 
 
 depart = 'reyk'
@@ -9,6 +24,4 @@ date = '190919'
 pattern_url = f'https://www.skyscanner.ru/transport/flights/{depart}/{arrive}/{date}/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&ref=home&currency=USD#results'
 
 if __name__ == "__main__":
-    source = get_response(pattern_url)
-    flights = get_flights_details(source)
-    print(flights)
+    sched.start()
