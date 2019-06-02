7e376251c1f4ac24f42d39cdc2faf85e898d8fec
Vlad Goenko <vlad.goenko@gmail.com>
2019-06-02 18:22:21 +0300

    test Heroku printing mechanism 2.0

--- scanner.py
+++ scanner.py
@@ -12,8 +12,8 @@ sched = BlockingScheduler()
 def timed_job():
     # source = get_response(pattern_url)
     # flights = get_flights_details(source)
-    # print(flights)
     print('Hello, world! {}'.format(datetime.datetime.now()))
+    # print(flights)
     sys.stdout.flush()
 
 
@@ -24,4 +24,5 @@ date = '190919'
 pattern_url = f'https://www.skyscanner.ru/transport/flights/{depart}/{arrive}/{date}/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&ref=home&currency=USD#results'
 
 if __name__ == "__main__":
+    print('clock is going to start {}'.format(datetime.datetime.now()))
     sched.start()
